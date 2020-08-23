10ca911d53e470aade76fbdece74d11c95bb8b37
DifficultShadow <65045486+DifficultShadow@users.noreply.github.com>
2020-08-24 00:40:07 +0200

    Respawn and checkpoint
    here you click Space bar to kill the turtle when the turtle die if this turtle passed by a checkpoint and then after the turtle passed by a checkpoint and touched it then the turtle will respawn at that checkpoint

---
+++ Assets/PlayerPos.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerPos : MonoBehaviour
+{
+    private GameManager gm;
+
+    private void Start()
+    {
+        gm = GameObject.FindGameObjectWithTag("GM").GetComponent<GameManager>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            Debug.Log("LevelRestarted");
+        }
+    }
+}
